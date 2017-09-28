f0b0b9519a11bccd3b62ed5eb39e4ce1aecf33be
y <yasu.ellegarden@gmail.com>
2017-09-28 17:57:05 +0900

    途中

---
+++ HelloPython/Test.py
@@ -0,0 +1,40 @@
+# coding= UTF-8
+'''
+Created on 2017/09/28
+
+@author: y
+'''
+print("こんにちは," + "Python");
+print("Hello," + "Python");
+
+#変数xを定義
+x = 5
+print(x)
+x = 11
+print(x)
+
+x = x + 5
+print(x)
+
+money = 2000
+print(money)
+#変数moneyに5000を足して、変数moneyを上書きする
+money += 5000
+
+#変数moneyの値を出力
+print(money)
+
+price = 100
+print('りんごの価格は' + str(price) + '円です')
+
+count = '3'
+total_price = price * int(count)
+print(total_price)
+
+age = 24
+print('私は' + str(age) + '歳です')
+
+count = '5'
+print(int(count) + 1)
+
+
